33206122972c1f3f41d1932718f979f1edad0ded
Anisbeflo <abejarano0@ucol.mx>
2022-03-05 16:09:07 -0600

    Exercise to sum numbers

---
+++ 01-Sum/add.js
@@ -0,0 +1,9 @@
+function add(a, b) {
+  var a = $("#num1").val();
+  var b = $("#num2").val();
+  var c = parseInt(a) + parseInt(b);
+  if (isNaN(b) || isNaN(a))
+    $("#total").val('NaN');
+  else
+    $("#total").val(c);
+};
